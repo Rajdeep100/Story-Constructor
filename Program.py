dfc093552a22bcdd115bc42eb6f010df68e5122d
Rajdeep Thakare <32894866+Rajdeep100@users.noreply.github.com>
2019-05-08 18:18:16 +0530

    Add files via upload

---
+++ Program.py
@@ -0,0 +1,14 @@
+#STORY PROGRAM
+uname = input("Enter Your Nick-Name: ")
+pname = input("Enter The Name Of Your Favourite Animal: ")
+cname = input("Enter The Name Of Your City: ")
+#fname = input("Enter your Friend's Name: ")
+#======================================================================================================================================================================================
+print("==================================================================================================================================================================================")
+print("""Once upon a time, A salt seller named""",uname,"""used to carry the salt bag on his""",pname,"""to the""",cname,"""market every day.
+On the way they had to cross a river. One day the""",pname,"""suddenly tumbled down the river and the salt bag also fell into the water.
+The salt dissolved in the water and hence the bag became very light to carry. The""",pname,"""was happy.
+Then the""",pname,"""started to play the same trick every day.
+Now,""",uname,"""came to understand the trick and decided to teach a lesson to it. The next day he loaded a cotton bag on the""",pname,""".
+Again it played the same trick hoping that the cotton bag would be still become lighter.
+But the dampened cotton became very heavy to carry and the""",pname,"""suffered. It learnt a lesson. It didn’t play the trick anymore after that day, and""",uname,"""was happy.""")
